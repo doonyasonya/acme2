b62c49fc01ecad0d68b2dec2a1ab82d540569f05
doonyasonya <141338994+doonyasonya@users.noreply.github.com>
2024-04-08 12:16:07 +0300

    Add files via upload

---
+++ acme_project/urls.py
@@ -0,0 +1,14 @@
+# acme_project/urls.py
+# Импортируем настройки проекта.
+from django.conf import settings
+# Импортируем функцию, позволяющую серверу разработки отдавать файлы.
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('', include('pages.urls')),
+    path('admin/', admin.site.urls),
+    path('birthday/', include('birthday.urls')),
+    # В конце добавляем к списку вызов функции static.
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ pages/urls.py
@@ -0,0 +1,10 @@
+# pages/urls.py
+from django.urls import path
+
+from . import views
+
+app_name = 'pages'
+
+urlpatterns = [
+    path('', views.HomePage.as_view(), name='homepage'),
+]
